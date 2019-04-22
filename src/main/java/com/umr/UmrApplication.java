8cbf0f1958b3876f999f4155e0944e7104b5f51b
tengjing21957 <tengjing21957@hsjry.com>
2019-04-22 09:48:20 +0800

    init

---
+++ src/main/java/com/umr/UmrApplication.java
@@ -0,0 +1,13 @@
+package com.umr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmrApplication.class, args);
+	}
+
+}
